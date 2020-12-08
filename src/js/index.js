828b885cdc664b3bb293d38a3bf9c40014960e32
SushyDev <xsushy@hotmail.com>
2020-12-08 22:26:41 +0100

    final version

--- src/js/index.js
+++ src/js/index.js
@@ -13,16 +13,15 @@ const topAppBar = new MDCTopAppBar(topAppBarElement);
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
 
 // Instantiation
-const listEl = document.querySelector('.mdc-drawer .mdc-list');
+const listElem = document.querySelector('.mdc-drawer .mdc-list');
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 
-//Add event listeners
-listEl.addEventListener('click', (event) => {
-    goto(event);
+//If click list element close drawer
+listElem.addEventListener('click', (event) => {
     drawer.open = !drawer.open;
 });
 
-//If drawr is open close it
+//If drawer is open close it
 topAppBar.setScrollTarget(document.getElementById('main-content'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
     drawer.open = !drawer.open;
